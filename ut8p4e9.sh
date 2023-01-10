4691b0870b74c1e74f1447a85de61ce54fc3c11f
Miquel35 <alumar1745@ieselcaminas.org>
2023-01-10 13:50:04 +0100

    Ejercicios UT8P4

---
+++ ut8p4e9.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Realiza un script que cada vez que se ejecute genere una copia de seguridad del
+#directorio /etc en el fichero /root/copia_etc_fecha.tgz (indicando la fecha cuándo se ha
+#generado la copia de seguridad).
+
+fecha=`date +%d-%m-%Y`
+rsync -az /etc /root/copia_etc_$fecha.tgz
+
